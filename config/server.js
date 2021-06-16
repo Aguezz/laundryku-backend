1ab0578de0416f865d624b6d0c6d5788fc2a5e99
Agus Stiawan <agus.stiawan2211@gmail.com>
2021-06-16 08:14:28 +0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '53ff6b6e8cb4236287e0801ea4bf0398'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b4531c6f-889b-48e8-a75e-b0fd18f41134'
+};
